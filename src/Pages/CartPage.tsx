7e537b1e6d0d0942c21a9e436a9a24f0f592c0a8
Adarsh Singh <singhadarsh9540@gmail.com>
2025-01-24 20:06:25 +0530

    feat: vercel.json is added

--- src/Pages/CartPage.tsx
+++ src/Pages/CartPage.tsx
@@ -1,5 +1,4 @@
-
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import { Cart } from "../components/Cart";
 import { ShoppingBag } from "lucide-react";
 
@@ -7,7 +6,7 @@ export function CartPage() {
   const navigate = useNavigate();
 
   return (
-    <div className="min-h-screen bg-gray-50 my-20">
+    <div className="min-h-screen bg-gray-50 py-10">
       <div className="max-w-3xl mx-auto px-4 py-12">
         <div className="flex items-center space-x-3 mb-8">
           <div className="w-10 h-10 bg-green-100 rounded-full flex items-center justify-center">
